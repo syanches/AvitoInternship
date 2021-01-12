13213c0f8e2ec4ca7e881e7e451d915586e8fce3
Artem Martirosyan <syanches@gmail.com>
2021-01-12 23:01:11 +0300

    initial

---
+++ AvitoInternship/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AvitoInternship
+//
+//  Created by Artem Martirosyan on 12.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
